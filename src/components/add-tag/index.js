25f54a54db8227011ca94bdc16544db214d19854
Sameeksha Jain <sameekshajn9@gmail.com>
2019-12-15 22:46:19 +0530

    - color fix

--- src/components/add-tag/index.js
+++ src/components/add-tag/index.js
@@ -2,7 +2,9 @@ import React from "react"
 
 export default () => (<div>
     <button class="button tagButton">
-        Interview<i class="fa fa-close"></i>
+    <span style={{
+            fontWeight: 500
+        }}>Interview</span><i class="fa fa-close"></i>
     </button>
     <button class="button addButton" style={{ marginLeft: 10 }}>
         <span style={{
